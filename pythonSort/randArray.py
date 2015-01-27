d1ebc77174fcfbc1262b7b7f11598aa7804bd29a
bIgBV <bhargav.v2@gmail.com>
2015-01-27 21:06:02 +0530

    Added insertion sort and a general purpose random array generator

---
+++ pythonSort/randArray.py
@@ -0,0 +1,19 @@
+#!/bin/pyhton3
+"""
+Simple script that returns a list of size n filled with random values within 
+a provided limit
+"""
+import random
+
+def createRandArray(n, lim):
+    temp = []
+    for i in range(n):
+        temp.append(random.randrange(lim))
+    return temp
+
+"""
+Usage
+"""
+
+if __name__ == '__main__':
+    print(createRandArray(10, 100))
