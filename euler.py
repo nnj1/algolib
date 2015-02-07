948a27f2d623c9af84afd5836a7811e0158eb68c
Newlyn Joseph <shrimpboyhoftw@gmail.com>
2015-02-06 22:15:31 -0500

    Update euler.py

--- euler.py
+++ euler.py
@@ -5,11 +5,10 @@ class Euler:
         pass
     def approx(self, func, t, y, step, iters):
         data = []
-        step = 0.1
         i = 0
         while i <= iters:
             slope = func(t,y)
-            y += step * slope
+            y += (step * slope)
             data.append([i,t,y,slope])
             t += step
             i += 1
@@ -17,8 +16,8 @@ class Euler:
 euler = Euler()
 
 # usage
-# Euler.approx(equation, x0, y0, stepsize, maxiterations)
-data = euler.approx(lambda t,y : y**2 - y**3, 0, 0.2, 0.1, 100)
+# Euler.approx(y' = f(t,y), x0, y0, stepsize, maxiterations)
+data = euler.approx(lambda t,y : 2*(y**3) + t**2, 0, -0.5, 0.1, 20)
 
 print 'Iter\tt\ty\tm'
 pp = pprint.PrettyPrinter(indent=0)
